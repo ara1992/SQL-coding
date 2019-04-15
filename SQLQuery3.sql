6ee89690a4c404dc888ced2f548def2117a3257a
ara1992 <49656181+ara1992@users.noreply.github.com>
2019-04-16 03:03:18 +0800

    Update SQLQuery3.sql

--- SQLQuery3.sql
+++ SQLQuery3.sql
@@ -5,7 +5,7 @@ select diff as 'Date_Interval_Since_Prev_Order',count([OrderID]) as 'Order_Count
                     lag([OrderDate],1) over (partition by [CustomerID] order by [OrderID]),
                     [OrderDate]
                    ) as diff
-    from  dbo.Salesorder$ 
+    from  dbo.Salesorder
       ) as TT where  diff>0
 	  group by diff
 	  order by count([OrderID]) desc
